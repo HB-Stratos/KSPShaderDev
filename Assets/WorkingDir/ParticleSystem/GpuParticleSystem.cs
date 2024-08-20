9673573550789cb43d918ede8847e712d9a71351
HB-Stratos <56313836+HB-Stratos@users.noreply.github.com>
2024-08-20 02:10:37 +0200

    First glimpses of a particle system, and reflection to dispose buffers!

---
+++ Assets/WorkingDir/ParticleSystem/GpuParticleSystem.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+public class GpuParticleSystem : MonoBehaviour
+{
+    [Header("Particle System Variables")]
+    public Vector3 emitVector = Vector3.forward;
+
+    public bool isBillboard = true;
+
+    [Range(1, 1_000_000)]
+    public int maxParticleCount = 100_000;
+
+    [Header("To be replaced with procedural gets")]
+    public Mesh particleMesh;
+    public ComputeShader computeShader;
+    public Shader particleShader;
+
+    // Particle:
+    // - Position
+    // - Velocity
+
+
+    ComputeBuffer particlePositions;
+    ComputeBuffer particleVelocies;
+
+    Material particleMaterial;
+    Bounds bounds;
+
+    int computeThreadGroups;
+
+    ComputeBuffer particleTriangles;
+    ComputeBuffer particleVertexPositions;
+
+    int particleUpdateKernel;
+
+    private List<ComputeBuffer> GetAllComputeBuffers()
+    {
+        var computeBufferFields = this.GetType()
+            .GetFields(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public)
+            .Where(p => p.FieldType == typeof(ComputeBuffer));
+        return computeBufferFields.Select(p => (ComputeBuffer)p.GetValue(this)).ToList();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleMaterial = new Material(particleShader);
+
+        particlePositions = new ComputeBuffer(maxParticleCount, sizeof(float) * 3);
+        particleVelocies = new ComputeBuffer(maxParticleCount, sizeof(float) * 3);
+
+        particleUpdateKernel = computeShader.FindKernel("UpdateParticle");
+        uint computeThreadGroupSize;
+        computeShader.GetKernelThreadGroupSizes(
+            particleUpdateKernel,
+            out computeThreadGroupSize,
+            out _,
+            out _
+        );
+        computeThreadGroups = (int)(
+            (maxParticleCount + (computeThreadGroupSize - 1)) / computeThreadGroupSize
+        );
+
+        int[] inputParticleTriangles = particleMesh.triangles;
+        particleTriangles = new ComputeBuffer(inputParticleTriangles.Length, sizeof(int));
+        particleTriangles.SetData(inputParticleTriangles);
+        Vector3[] inputParticleVertexPositions = particleMesh.vertices;
+        particleVertexPositions = new ComputeBuffer(
+            inputParticleVertexPositions.Length,
+            sizeof(float) * 3
+        );
+        particleVertexPositions.SetData(inputParticleVertexPositions);
+
+        computeShader.SetVector("_EmitVector", emitVector); //actually sets a vec4, but with w=0;
+        computeShader.SetBuffer(particleUpdateKernel, "_ParticlePositions", particlePositions);
+        computeShader.SetBuffer(particleUpdateKernel, "_ParticleVelocities", particleVelocies);
+
+        bounds = new Bounds(Vector3.zero, Vector3.one * 1_000_000);
+
+        particleMaterial.SetBuffer("_ParticlePositions", particlePositions);
+        particleMaterial.SetBuffer("_ParticleTriangles", particleTriangles);
+        particleMaterial.SetBuffer("_ParticleVertexPositions", particleVertexPositions);
+        particleMaterial.SetInt("_isBillboard", isBillboard ? 1 : 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        computeShader.SetFloat("unity_DeltaTime", Time.deltaTime);
+        computeShader.Dispatch(particleUpdateKernel, computeThreadGroups, 1, 1);
+
+        Graphics.DrawProcedural(
+            particleMaterial,
+            bounds,
+            MeshTopology.Triangles,
+            particleTriangles.count,
+            maxParticleCount
+        );
+    }
+
+    void OnDestroy()
+    {
+        List<ComputeBuffer> test = this.GetAllComputeBuffers();
+        foreach (ComputeBuffer computeBuffer in GetAllComputeBuffers())
+        {
+            computeBuffer.Dispose();
+        }
+    }
+}
---
+++ Assets/WorkingDir/ParticleSystem/ParticleRendering.shader
@@ -0,0 +1,57 @@
+﻿Shader "Hidden/ParticleRendering"
+{
+    //show values to edit in inspector
+    Properties
+    {
+        [HDR] _Color ("Tint", Color) = (0, 0, 0, 1)
+    }
+
+    SubShader
+    {
+        //the material is completely non-transparent and is rendered at the same time as the other opaque geometry
+        Tags { "RenderType" = "Opaque" "Queue" = "Geometry" }
+
+        Pass
+        {
+            CGPROGRAM
+
+            //include useful shader functions
+            #include "UnityCG.cginc"
+
+            //define vertex and fragment shader functions
+            #pragma vertex vert
+            #pragma fragment frag
+
+            //tint of the texture
+            fixed4 _Color;
+
+            //buffers
+            StructuredBuffer<float3> _ParticlePositions;
+            StructuredBuffer<int> _ParticleTriangles;
+            StructuredBuffer<float3> _ParticleVertexPositions;
+            int _IsBillboard;
+
+            //the vertex shader function
+            float4 vert(uint vertex_id : SV_VertexID, uint instance_id : SV_InstanceID) : SV_POSITION
+            {
+                //get vertex position
+                int positionIndex = _ParticleTriangles[vertex_id];
+                float3 position = _ParticleVertexPositions[positionIndex];
+                //add sphere position
+                position += _ParticlePositions[instance_id];
+                //convert the vertex position from world space to clip space
+                return mul(UNITY_MATRIX_VP, float4(position, 1));
+            }
+
+            //the fragment shader function
+            fixed4 frag() : SV_TARGET
+            {
+                //return the final color to be drawn on screen
+                return _Color;
+            }
+            
+            ENDCG
+        }
+    }
+    Fallback "VertexLit"
+}
