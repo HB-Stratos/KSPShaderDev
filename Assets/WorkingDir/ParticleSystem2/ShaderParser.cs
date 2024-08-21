e1b287f62e57bc1297108606e06feb793188ff7a
HB-Stratos <56313836+HB-Stratos@users.noreply.github.com>
2024-08-22 00:43:06 +0200

    trying to re-write the shader parser (unfinished)

---
+++ Assets/WorkingDir/ParticleSystem2/ShaderParser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityShaderParser.HLSL;
+using UnityShaderParser.HLSL.PreProcessor;
+
+public static class ShaderParser
+{
+    // Start is called before the first frame update
+    public static int? GetHLSLShaderStructBufferSize(string path)
+    {
+        string shaderFileContent = LoadTextFileFromPath(path);
+        if (String.IsNullOrEmpty(shaderFileContent))
+            throw new FileNotFoundException();
+
+        var tokens = HLSLLexer.Lex(shaderFileContent, null, null, false, out _);
+
+        var parsed = HLSLParser.ParseTopLevelDeclarations(
+            tokens,
+            new HLSLParserConfig() { PreProcessorMode = PreProcessorMode.StripDirectives },
+            out _,
+            out _
+        );
+
+        var particleStructNode = parsed
+            .Where(node => node.GetType() == typeof(StructDefinitionNode))
+            .Where(node =>
+                node.Tokens.First(token => token.Kind == TokenKind.IdentifierToken).Identifier
+                == "Particle"
+            )
+            .ToArray()[0];
+        var particleStructMembers = particleStructNode
+            .Tokens.Where(token => Enum.GetName(typeof(TokenKind), token.Kind).Contains("Keyword"))
+            .ToArray();
+
+        var test = HLSLParser.ParseExpression(
+            particleStructNode.Tokens,
+            new HLSLParserConfig(),
+            out _,
+            out _
+        );
+
+        var test2 = test.GetPrettyPrintedCode();
+
+        return null;
+    }
+
+    private static string LoadTextFileFromPath(string path)
+    {
+        return File.Exists(path) ? File.ReadAllText(path) : null;
+    }
+}
---
+++ Assets/WorkingDir/ParticleSystem2/TestThingEditor.cs
@@ -0,0 +1,17 @@
+﻿using TMPro;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[CustomEditor(typeof(TestThing))]
+public class TestThingEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Run test code"))
+            ShaderParser.GetHLSLShaderStructBufferSize(
+                "Assets/WorkingDir/ParticleSystem2/TestParticle.compute"
+            );
+    }
+}
